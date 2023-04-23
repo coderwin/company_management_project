d47ca311ec90712d3fb988bb6c4346d82d32e125
coderwin <gommind@naver.com>
2023-04-23 20:16:46 +0900

    create backend project

---
+++ companymanagement_backend/src/main/java/com/example/companymanagement_backend/CompanymanagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.companymanagement_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanymanagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanymanagementBackendApplication.class, args);
+	}
+
+}
---
+++ companymanagement_backend/src/test/java/com/example/companymanagement_backend/CompanymanagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companymanagement_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanymanagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
