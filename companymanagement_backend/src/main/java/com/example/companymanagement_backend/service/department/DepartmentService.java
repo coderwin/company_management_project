cb149dffae4935f69329b570cbf7fbead9af9203
coderwin <gommind@naver.com>
2023-04-25 15:27:48 +0900

    update service of department

--- companymanagement_backend/src/main/java/com/example/companymanagement_backend/service/department/DepartmentService.java
+++ companymanagement_backend/src/main/java/com/example/companymanagement_backend/service/department/DepartmentService.java
@@ -1,5 +1,10 @@
 package com.example.companymanagement_backend.service.department;
 
+import com.example.companymanagement_backend.web.department.DepartmentDetailForm;
+import com.example.companymanagement_backend.web.department.DepartmentListForm;
+
+import java.util.List;
+
 /**
  * Department Service
  * writer : 이호진
@@ -9,4 +14,23 @@ package com.example.companymanagement_backend.service.department;
  * description : Department Service 메소드 모음
  */
 public interface DepartmentService {
+
+    /**
+     * writer : 이호진
+     * init : 2023.04.25
+     * updated by writer :
+     * update :
+     * description : 부서 및 위치 정보 불러오기
+     *               id : 부서 아이디
+     */
+    DepartmentDetailForm watchDetail(Long id);
+
+    /**
+     * writer : 이호진
+     * init : 2023.04.25
+     * updated by writer :
+     * update :
+     * description : 모든 부서 불러오기
+     */
+    List<DepartmentListForm> selectList();
 }
