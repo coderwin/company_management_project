28fa016f080b49eba4a5ea2139c1d2018bda294b
coderwin <gommind@naver.com>
2023-04-27 13:00:39 +0900

    update app

--- companymanagement_front/src/App.js
+++ companymanagement_front/src/App.js
@@ -10,34 +10,47 @@ import EmployeeJobHistory from './pages/employee/EmployeeJobHistory';
 import DepartmentList from './pages/department/DepartmentList';
 import DepartmentDetail from './pages/department/DepartmentDetail';
 import DepartmentSalaryUpdate from './pages/department/DepartmentSalaryUpdate';
+import { createContext, useState } from 'react';
+
+// context 만들기
+export const CustomContext = createContext(null);
 
 function App() {
+
+  /// 변수 모음
+  const serverHost = "http://localhost:8080";// 서버 host
+
+  /// 상태 모음
+  const [loding, setLoding] = useState(false);// 요청 처리
+
   return (
     <>
-      {/* header */}
-      <Header />
+      <CustomContext.Provider value={{serverHost, loding, setLoding}}>
+        {/* header */}
+        <Header />
 
-      {/* body */}
-      <Routes>
+        {/* body */}
+        <Routes>
 
-        {/* employee */}
-        <Route path="/employee/list" element={<EmployeeList />} /> 
-        <Route path="/employee/:num" elemnet={<EmployeeDetail />} />
-        <Route path="/employee/:num/jobhistory" element={<EmployeeJobHistory />} />
+          {/* employee */}
+          <Route path="/employee/list" element={<EmployeeList />} /> 
+          <Route path="/employee/:num" elemnet={<EmployeeDetail />} />
+          <Route path="/employee/:num/jobhistory" element={<EmployeeJobHistory />} />
 
-        {/* department */}
-        <Route path="/department/list" element={<DepartmentList />} /> 
-        <Route path="/deparment/:num" element={<DepartmentDetail />} />
-        <Route path="/department/update/salary" elemnet={<DepartmentSalaryUpdate />} />
+          {/* department */}
+          <Route path="/department/list" element={<DepartmentList />} /> 
+          <Route path="/deparment/:num" element={<DepartmentDetail />} />
+          <Route path="/department/update/salary" elemnet={<DepartmentSalaryUpdate />} />
 
-        {/* 404 page */}
-        <Route path="/*" element={<NotFound />} />
-        {/* 클라이언트의 잘못된 요청 */}
-        <Route path="/errors/notfound" element={<NotFound />} />
-      </Routes>
-        
-      {/* footer */}
-      <Footer />
+          {/* 404 page */}
+          <Route path="/*" element={<NotFound />} />
+          {/* 클라이언트의 잘못된 요청 */}
+          <Route path="/errors/notfound" element={<NotFound />} />
+        </Routes>
+          
+        {/* footer */}
+        <Footer />
+      </CustomContext.Provider>
     </>
   );
 }
